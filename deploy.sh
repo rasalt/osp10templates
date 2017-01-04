c4e20d422322f11d905a7ed25deef8a8344094ae
Ruchika K <ruchika.kharwar@gmail.com>
2017-01-03 22:36:52 -0500

    tenant traffic on its own network
    Signed-off-by: Ruchika K <ruchika.kharwar@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,12 @@
-openstack overcloud deploy --templates /usr/share/openstack-tripleo-heat-templates/mitaka/ -e /home/stack/templates/advanced-networking.yaml   --compute-flavor compute --control-flavor control --compute-scale 1 --control-scale 1 --ceph-storage-scale 0 --neutron-network-type vxlan --neutron-tunnel-types vxlan --ntp-server pool.ntp.org
+#This architecture has the following details:
+#Controller node
+#NIC1- management
+#NIC2- external
+#NIC3- tenant
+
+#Compute node
+#NIC1- management
+#NIC2- tenant
+#Want tenant traffic to be on its own interface br-vlan on NIC2
+
+openstack overcloud deploy --templates -e /home/stack/templates/advanced-networking.yaml   --compute-flavor compute --control-flavor control --compute-scale 1 --control-scale 1 --ceph-storage-scale 0 --neutron-network-type vxlan --neutron-tunnel-types vxlan --ntp-server pool.ntp.org --neutron-bridge-mappings datacentre:br-ex,tenant:br-vlan --neutron-network-vlan-ranges tenant:30:100

67eaa3321fea3fba01ac056a462b75b2dba7338f
Ruchika K <ruchika.kharwar@gmail.com>
2017-01-06 00:23:02 -0500

    Works for tenant vlans
    Signed-off-by: Ruchika K <ruchika.kharwar@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,4 @@
 #NIC2- tenant
 #Want tenant traffic to be on its own interface br-vlan on NIC2
 
-openstack overcloud deploy --templates -e /home/stack/templates/advanced-networking.yaml   --compute-flavor compute --control-flavor control --compute-scale 1 --control-scale 1 --ceph-storage-scale 0 --neutron-network-type vxlan --neutron-tunnel-types vxlan --ntp-server pool.ntp.org --neutron-bridge-mappings datacentre:br-ex,tenant:br-vlan --neutron-network-vlan-ranges tenant:30:100
+openstack overcloud deploy --templates -e /home/stack/templates/advanced-networking.yaml   --compute-flavor compute --control-flavor control --compute-scale 1 --control-scale 1 --ceph-storage-scale 0 --neutron-network-type vlan --neutron-tunnel-types vxlan,vlan --ntp-server pool.ntp.org --neutron-bridge-mappings datacentre:br-ex,tenant:br-vlan --neutron-network-vlan-ranges tenant:30:100
